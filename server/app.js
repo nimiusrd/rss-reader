13632749902b7fc063cae1faa683dc0c8efe9655
Yudai Udagawa <nimius.rd@gmail.com>
2017-04-04 16:18:36 +0900

    Add API server

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+// Get Feedly developer access tokens from https://feedly.com/v3/auth/dev
+const client_id = process.env.FEEDLY_CLIENT_ID
+const auth = process.env.FEEDLY_AUTHORIZATION
+
+const app = new express()
+const option = {
+    method: 'GET',
+    client_id: client_id,
+    headers: {
+        Authorization: auth
+    }
+}
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get('/feed', (req, res) => {
+    const feedid = encodeURIComponent('feed/http://html5experts.jp/feed/')
+    fetch(`https://cloud.feedly.com/v3/streams/${feedid}/contents`, option)
+        .then(res => res.json())
+        .then(json => {
+            const result = json.items.map(item => {
+                return {
+                    link: item.alternate[0].href,
+                    description: item.content.content,
+                    title: item.title
+                }
+            })
+            res.send(result)
+        })
+})
+
+app.listen(8080)
--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import {FeedItem} from './components/FeedItem';
 
-export const App = () => (
-  <div>
-    <FeedItem
-      description="piyopiyo"
-      link="example.com"
-      title="hoge"
-    />
-    <FeedItem
-      description=""
-      link=""
-      title=""
-    />
-    <FeedItem />
-  </div>
-);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feedItems: []
+    }
+    this.getFeed = this.getFeed.bind(this)
+  }
+
+  getFeed() {
+    fetch('http://localhost:8080/feed')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          feedItems: json
+        });
+      })
+  }
+
+  render() {
+    const feedItems = this.state.feedItems.map((item, index) => (
+      <FeedItem
+        key={index}
+        description={item.description}
+        link={item.link}
+        title={item.title}
+      />
+    ))
+    return (
+      <div>
+        <button onClick={this.getFeed}>Click me!</button>
+        {feedItems}
+      </div>
+    )
+  }
+}
