8e81fa67c303848f19df372a6380a8a561169eb2
Yudai Udagawa <nimius.rd@gmail.com>
2017-04-04 16:14:00 +0900

    Fix <a>'s attribute

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -4,8 +4,8 @@ import styles from './FeedItem.css';
 export const FeedItem = ({title, link, description}) =>
   <section className={styles.container}>
     <header>{title || 'no title'}</header>
-    <p>{description || 'no description'}</p>
-    <a>{link || 'no link'}</a>
+    <details>{description || 'no description'}</details>
+    <a href={link}>{link || 'no link'}</a>
   </section>;
 FeedItem.propTypes = {
   description: PropTypes.string.isRequired,
